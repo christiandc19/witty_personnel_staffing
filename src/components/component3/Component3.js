c144f3bd24d4769de87401249579aff8e19aed43
github_christiandc19 <christiandc19@hotmail.com>
2024-08-07 12:24:21 -0700

    styling

--- src/components/component3/Component3.js
+++ src/components/component3/Component3.js
@@ -23,7 +23,7 @@ const Component3 = () => {
 
             <p>
             At Witty Personnel, we are passionate about connecting healthcare facilities with exceptional professionals. Our mission is to enhance the quality of patient care and operational efficiency through comprehensive staffing solutions tailored to the unique needs of each client.</p>
-            <p>With years of industry experience, our team of dedicated recruiters and healthcare experts specializes in sourcing, vetting, and placing highly qualified medical and allied health professionals. We cover a wide range of specialties, including nursing, dental, optometry, mental health, telehealth, biotechnology, physical therapy, behavioral health, and correctional health.</p>
+            <p>With years of industry experience, our team of dedicated recruiters and healthcare experts specializes in sourcing, vetting, and placing highly qualified medical and allied health professionals. We cover a wide range of specialties, including nursing, mental health, MS Technicians, telehealth, counselors, physical therapy, behavioral health, and correctional health.</p>
             <p>Our rigorous selection process ensures that every candidate not only meets the required credentials but also embodies our core values of compassion, integrity, and dedication to excellence. By partnering with Witty Personnel, healthcare facilities gain access to a reliable and skilled workforce, enabling them to maintain high standards of care and achieve operational excellence</p> <br/>
 
             <div className='c3-btn'>
