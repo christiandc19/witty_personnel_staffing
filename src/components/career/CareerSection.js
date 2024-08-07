983dbe3e980a425d836b007e9bca0b28e415ac83
github_christiandc19 <christiandc19@hotmail.com>
2024-08-07 12:07:04 -0700

    styling

--- src/components/career/CareerSection.js
+++ src/components/career/CareerSection.js
@@ -7,8 +7,9 @@ const CareerSection = () => {
     <>
 
         <div className='career-section'>
-            <div className='career-section-content'>
-                <iframe src="https://www.paycomonline.net/v4/ats/web.php/jobs?clientkey=E44C4EF7C1C8D4418D54B54DF1838F5C&fromClientSide=true" title='paycom' ></iframe>
+        <div className='career-section-content'>
+            <iframe src="https://www.paycomonline.net/v4/ats/web.php/jobs?
+clientkey=31A83E63E9D18C0B219298A8B824A606&fromClientSide=true" title='paycom'></iframe>
             </div>
         </div>
 
